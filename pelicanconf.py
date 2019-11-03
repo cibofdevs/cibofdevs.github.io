7f126d6faecc120cf4c9ec867f8485dd8f15f0e3
cibofdevs <cibofdev@gmail.com>
2019-11-03 11:50:30 +0700

    change theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -49,7 +49,7 @@ LINKEDIN = 'cibofdev'
 GITHUB = 'cibofdevs'
 TWITTER = '@cibofdev'
 
-CAREER_SUMMARY = 'I am a software developer currently working as a freelance. I have experienced working on web and android development.'
+CAREER_SUMMARY = 'Halo, Nama Saya Ahmad Wijaya. Saya merupakan Mahasiswa Fresh Graduate Tahun ini. Saya Memiliki Keahlian dibidang Web Development, Android Development dan IoT.'
 
 SKILLS = [
 	{
@@ -74,20 +74,20 @@ SKILLS = [
   	}
 ]
 
-PROJECT_INTRO = 'You can list your side projects or open source libraries in this section. '
+PROJECT_INTRO = 'Berikut beberapa project yang pernah saya kerjakan. '
 
 PROJECTS = [
 	{
-		'title': 'Open Source Contributions',
-		'tagline': 'Active contributor in FOSSASIA, worked on the Open Event project (both server and android app).Active contributor in CLTK, worked on the CLTK Web app and API.Made valuable contributions in phpBB, implemented a live search feature.Also made a few contributions to Processing.org and phpMyAdmin.'
+		'title': 'Company Profile',
+		'tagline': 'Web company profile orange chicken.'
 	},
 	{
-		'title': 'Music Hub',
-		'tagline': 'Android app that connects multiple devices via wifi and plays music in all connected devices simultaneously to create a loud stereo-like sound effect.'
+		'title': 'Android E-Commerce',
+		'tagline': 'Aplikasi android untuk memudahkan transaksi jual dan beli dengan sistem B2C (Business to Customer).'
 	},
 	{
-		'title': 'Music Timer',
-		'tagline': 'Android app that monitors phone’s movement to detect whether the user’s asleep and pause music playback accordingly.'
+		'title': 'Sistem Monitoring Bebasis IoT',
+		'tagline': 'Project monitoring suhu berbasis website terintegrasi dengan perangkat IoT (Internet of Things).'
 	}
 ]
 
@@ -109,28 +109,16 @@ INTERESTS = [
 
 EXPERIENCES = [
 	{
-		'job_title': 'Software Development Engineer',
-		'time': 'Oct 2016 - Present',
-		'company': 'Zomato, Gurgaon IN',
-		'details': 'Part of the web team working on developing a smart POS system for restaurants.'	
+		'job_title': 'Web Developer',
+		'time': 'Aug 2019 - Present',
+		'company': 'Orange Chicken',
+		'details': 'Mengembangkan website company profile orange chicken cirebon.'	
 	},
 	{
-		'job_title': 'Web developer',
-		'time': 'Aug 2016 - Dec 2016',
-		'company': 'Archimedes Digital, WFH',
-		'details': 'Worked on developing an online catalog for museums using scans of various objects (artifacts, books etc). Developed a highly zoomable image viewer and integrated linting and test suites to be used across projects.'
-	},
-	{
-		'job_title': 'Google Summer of Code Student',
-		'time': 'May 2016 - Aug 2016',
-		'company': 'Classical Language Toolkit (CLTK), WFH',
-		'details': 'Worked on the CLTK webapp - a modern reading environment to study classical languages. Updated the webapp to provide definitions, translations and commentary along with the text. Added functionality to add annotations and bookmarks to enhance the reading experience for user. Working on extending user profile to include annotations, bookmarks and other social networks.'	
-	},
-	{
-		'job_title': 'Research and Development Intern',
-		'time': 'May 2015 - June 2015',
-		'company': 'Aspiring Minds, Gurgaon IN',
-		'details': 'Developed a CRM-simulation module integrated into the employability assessment platform AMCAT. Created the frontend for the module and wrote backend code for question delivery and scoring. Designed the database schemas for the module compatible with the current platform’s database.'
+		'job_title': 'Web Developer',
+		'time': 'Okt 2019 - Present',
+		'company': 'IPIEMS Cirebon',
+		'details': 'Mengembangkan website sistem informasi akademik.'	
 	}
 ]
 
