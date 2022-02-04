3239b49d9034ec90739d0df72bd346617d9c775e
Ahmad Wijaya <cibofdev@gmail.com>
2022-02-04 20:33:39 +0700

    fix color and toggle font dark mode

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function toggleDarkLight() {
     var body = document.getElementById("body");
     var currentClass = body.className;
     body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
-    var text = document.getElementById("switchtext");
-    var currentText = text.className;
-    text.className = currentClass == "dark-mode" ? "fas fa-moon fa-2x" : "fas fa-sun fa-2x";
+    var span = document.getElementById("switchtext");
+    span.className = currentClass == "dark-mode" ? "fas fa-moon fa-2x" : "fas fa-sun fa-2x";
 }
