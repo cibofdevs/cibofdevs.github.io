272d58651fa4b4ca33b87c1ff99dfd40d343d6e9
Ahmad Wijaya <cibofdev@gmail.com>
2022-02-04 18:31:42 +0700

    add toggle dark mode

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Hello 🌎! I'm, Ahmad.");
+
+function toggleDarkLight() {
+    var body = document.getElementById("body");
+    var currentClass = body.className;
+    body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+    var text = document.getElementById("switchtext");
+    var currentText = text.className;
+    text.className = currentClass == "dark-mode" ? "fas fa-moon fa-2x" : "fas fa-sun fa-2x";
+}
