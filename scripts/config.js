ad2d4670efcec9e721538a1c1170f4c2b227058e
ahmadwijaya <cibofdev@gmail.com>
2025-01-29 10:23:53 +0400

    fix repository secrets

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,10 @@
-const config = {
+const config = window.config || {
     EMAILJS_PUBLIC_KEY: '$EMAILJS_PUBLIC_KEY',
-    EMAILJS_SERVICE_ID: '$EMAILJS_SERVICE_ID',
+    EMAILJS_SERVICE_ID: '$EMAILJS_SERVICE_ID', 
     EMAILJS_TEMPLATE_ID: '$EMAILJS_TEMPLATE_ID'
 };
+
+// Prevent modification of config object
+Object.freeze(config);
+
+export default config;
