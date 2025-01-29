e9b72b6677efb1194d47b7af057cb92350c503b1
cibofdevs <cibofdev@gmail.com>
2025-01-29 05:44:09 +0000

    Deploying to gh-pages from @ cibofdevs/cibofdevs.github.io@5817899b476d7287ef05df428e1cf27b13ccb51e 🚀

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
 const config = {
-  EMAILJS_PUBLIC_KEY: 'T46s1dBcv8kvHiV0p',
-  EMAILJS_SERVICE_ID: 'service_xjg06j7',
-  EMAILJS_TEMPLATE_ID: 'template_0knro87'
+  EMAILJS_PUBLIC_KEY: "T46s1dBcv8kvHiV0p",
+  EMAILJS_SERVICE_ID: "service_xjg06j7",
+  EMAILJS_TEMPLATE_ID: "template_0knro87"
 };
