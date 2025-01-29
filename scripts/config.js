cfacda9315e8f6e9496e130d3b6ce46738728412
ahmadwijaya <cibofdev@gmail.com>
2025-01-29 12:08:45 +0400

    update config

--- scripts/config.js
+++ scripts/config.js
@@ -1,7 +1,7 @@
 const config = {
-    EMAILJS_PUBLIC_KEY: "your_public_key",
-    EMAILJS_SERVICE_ID: "your_service_id", 
-    EMAILJS_TEMPLATE_ID: "your_template_id"
+    EMAILJS_PUBLIC_KEY: "T46s1dBcv8kvHiV0p",
+    EMAILJS_SERVICE_ID: "service_xjg06j7",
+    EMAILJS_TEMPLATE_ID: "template_0knro87"
 };
 
 export default config;
