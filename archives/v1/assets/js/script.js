362bcca8a54426fd4337853659247558733cd5e2
Ahmad Wijaya <cibofdev@gmail.com>
2026-06-23 16:47:13 +0400

    - remove archives - update the profile pics - add link, new project and certificate

--- archives/v1/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello 🌎! I'm, Ahmad.");
-
-function toggleDarkLight() {
-    var body = document.getElementById("body");
-    var currentClass = body.className;
-    body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
-    var span = document.getElementById("switchtext");
-    span.className = currentClass == "dark-mode" ? "fas fa-moon fa-2x" : "fas fa-sun fa-2x";
-}
